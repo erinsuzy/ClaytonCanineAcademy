98eeb80837d8aef764d7c585e4e604bbb6c82e9b
Erin Schneider <erinsschneider@gmail.com>
2024-05-15 17:34:11 -0500

    trying to link to github

---
+++ src/main/java/com/example/claytoncanineacademy/ClaytonCanineAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.example.claytoncanineacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaytonCanineAcademyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClaytonCanineAcademyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/claytoncanineacademy/ClaytonCanineAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.claytoncanineacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaytonCanineAcademyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
